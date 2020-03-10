3c01cde27e24831cf7f73418579849fa210153cc
m1kma <mika@mikamakela.net>
2020-03-10 20:32:39 +0200

    add lambda source and modify readme

--- README.md
+++ README.md
@@ -1,15 +1,20 @@
 # E-paper (e-ink) dashboard project for the ESP8266
-The backend service (separate AWS Lambda) is used to collect data from the various datasources such as weather service and news RSS feeds. The data is formatted to the JSON by the backend. The ESP8266 calls the backend service, parses the JSON string and displays the data in the e-paper display.
+The e-paper dashboard displays current weather and news on the e-ink display. The ESP8266 calls periodically the backend service (AWS Lambda), parses the data and displays the data in the e-paper display. 
 
-## Components
-- E-paper display model: Waveshare 5.83 inch 600x448 B&W
-- ESP8266 model: NodeMCU (Amica)
+The backend AWS Lambda collects the data from the various datasources such as the weather service and news RSS feeds. The Lambda returns the data as a JSON string. AWS infra (API Gateway) is deployed manually.
+
+## Electrical components
+- E-paper display: Waveshare 5.83 inch 600x448 B&W
+- ESP8266: NodeMCU (Amica)
 
 ## Libraries
 - GxEPD2_BW
 - ESP8266WiFi
 - WiFiClientSecure
 - ArduinoJson
+
+## AWS Python Lambda
+- epaper-dashboard-backend.py
 
 ## Credits
 Copyright (c) 2020 Mika Mäkelä
---
+++ epaper-dashboard-backend-lambda.py
@@ -0,0 +1,183 @@
+
+ # (c) Mika Mäkelä - 2020
+ # AWS Lambda backend for the ESP8266 E-paper (e-ink) dashboard project.
+
+import json
+import urllib3
+import xml.etree.ElementTree as ET
+import datetime
+from random import randint
+from random import seed
+import os
+
+http = urllib3.PoolManager()
+adafruitkey = os.environ['adafruitkey']
+adafruituri = os.environ['adafruituri']
+
+def lambda_handler(event, context):
+   
+    fmi_time_delta = datetime.datetime.now() - datetime.timedelta(minutes=60)
+    fmi_time = fmi_time_delta.replace(microsecond=0).isoformat()
+    fmi_url_observation = 'http://opendata.fmi.fi/wfs?request=getFeature&storedquery_id=fmi::observations::weather::simple&place=malmi,helsinki&timestep=30&maxlocations=1&starttime={0}&parameters=t2m,rh,ws_10min,wg_10min'.format(fmi_time)
+    fmi_url_forecast = 'http://opendata.fmi.fi/wfs?request=getFeature&storedquery_id=fmi::forecast::hirlam::surface::point::simple&place=malmi,helsinki&timestep=60&maxlocations=1&parameters=temperature'
+
+    responseBody = {
+        **fetch_fmi_observation(fmi_url_observation),
+        **fetch_fmi_forecast(fmi_url_forecast),
+        **fetch_adafruit(),
+        'rss':fetch_rss()
+    }
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(responseBody,  ensure_ascii=False)
+    }
+
+
+def fetch_fmi_observation(fmi_url_observation):
+    parRhTime = None
+    parRhValue = None
+    parTempTime = None
+    parTempValue = None
+    parWS10Value = None
+    parWG10Value = None
+
+    # HTTP Request FMI Observation 
+    fmi_response_obs = http.request('GET', fmi_url_observation)
+    fmi_xml_data_obs = fmi_response_obs.data
+    fmi_root_obs = ET.fromstring(fmi_xml_data_obs)
+        
+    for members in fmi_root_obs.findall('{http://www.opengis.net/wfs/2.0}member'):
+        for member in members.findall('{http://xml.fmi.fi/schema/wfs/2.0}BsWfsElement'):
+            
+            parName = member.find('{http://xml.fmi.fi/schema/wfs/2.0}ParameterName')
+            
+            if parName.text == 'rh':
+                parRhTime = member.find('{http://xml.fmi.fi/schema/wfs/2.0}Time').text
+                parRhValue = member.find('{http://xml.fmi.fi/schema/wfs/2.0}ParameterValue').text
+                
+            if parName.text == 't2m':
+                parTempValue = member.find('{http://xml.fmi.fi/schema/wfs/2.0}ParameterValue').text
+                
+            if parName.text == 'ws_10min':
+                parWS10Value = member.find('{http://xml.fmi.fi/schema/wfs/2.0}ParameterValue').text
+
+            if parName.text == 'wg_10min':
+                parWG10Value = member.find('{http://xml.fmi.fi/schema/wfs/2.0}ParameterValue').text
+
+    return {
+        'tempOut':parTempValue,
+        'rhOut':round(float(parRhValue)),
+        'ws10Out':parWS10Value,
+        'wg10Out':parWG10Value
+    }
+
+
+def fetch_fmi_forecast(fmi_url_forecast):
+    parTemp6hValue = None
+    parTemp12hValue = None
+    parTemp24hValue = None
+
+    # HTTP Request FMI Forecast 
+    fmi_response_forecast = http.request('GET', fmi_url_forecast)
+    fmi_xml_data_forecast = fmi_response_forecast.data
+    fmi_root_forecast = ET.fromstring(fmi_xml_data_forecast)
+    
+    itemcount = 0
+    
+    for members in fmi_root_forecast.findall('{http://www.opengis.net/wfs/2.0}member'):
+        for member in members.findall('{http://xml.fmi.fi/schema/wfs/2.0}BsWfsElement'):
+            
+            itemcount = itemcount + 1
+
+            parName = member.find('{http://xml.fmi.fi/schema/wfs/2.0}ParameterName')
+
+            if parName.text == 'temperature' and itemcount == 7:
+                parTemp6hValue = member.find('{http://xml.fmi.fi/schema/wfs/2.0}ParameterValue').text
+                
+            if parName.text == 'temperature' and itemcount == 13: 
+                parTemp12hValue = member.find('{http://xml.fmi.fi/schema/wfs/2.0}ParameterValue').text
+                
+            if parName.text == 'temperature' and itemcount == 25: 
+                parTemp24hValue = member.find('{http://xml.fmi.fi/schema/wfs/2.0}ParameterValue').text
+
+    return {
+        'temp6h':round(float(parTemp6hValue), 1),
+        'temp12h':round(float(parTemp12hValue), 1),
+        'temp24h':round(float(parTemp24hValue), 1)
+    }
+
+
+def fetch_rss():
+
+    rss_urls = []
+    
+    rss_urls.append(['Kauppalehti','https://feeds.kauppalehti.fi/rss/main'])
+    #rss_urls.append(['Helsingin Sanomat','https://www.hs.fi/rss/teasers/etusivu.xml'])
+    rss_urls.append(['Iltalehti','https://www.iltalehti.fi/rss/rss.xml'])
+    #rss_urls.append(['Tekniikka ja Talous','https://www.tekniikkatalous.fi/api/feed/v2/rss/tt'])
+    rss_urls.append(['Yle luetuimmat','https://feeds.yle.fi/uutiset/v1/mostRead/YLE_UUTISET.rss'])
+    #rss_urls.append(['Yle Uusimaa','https://feeds.yle.fi/uutiset/v1/recent.rss?publisherIds=YLE_UUTISET&concepts=18-147345'])
+  
+    randomint = randint(0, 2)
+    rss_final = rss_urls[randomint]
+
+    rss_response = http.request('GET', rss_final[1])
+    rss_xml_data = rss_response.data.decode('utf-8')
+    
+    #entire feed
+    rss_root = ET.fromstring(rss_xml_data)
+    rss_item = rss_root.findall('channel/item')
+
+    rss_feed = []
+
+    count = 0
+    
+    rss_feed.append(rss_final[0])
+    
+    for entry in rss_item:
+        count += 1
+        title = entry.findtext('title')  
+        rss_feed.append(title.replace('ä', 'a').replace('ö','o'))
+
+        if count > 5:
+            break
+
+    return rss_feed
+        
+
+def fetch_adafruit():
+
+    adafruit_temp_value = 0
+    adafruit_rh_value = 0
+    adafruit_tulo_temp_value = 0
+    adafruit_tulo_rh_value = 0
+    
+    adafruit_header = {'X-AIO-Key': adafruitkey}
+    adafruit_temp_url = '{0}/temperature/data/last'.format(adafruituri)
+    adafruit_rh_url = '{0}/humidity/data/last'.format(adafruituri)
+    adafruit_tulo_temp_url = '{0}/temperaturesauna/data/last'.format(adafruituri)
+    adafruit_tulo_rh_url = '{0}/humiditysauna/data/last'.format(adafruituri)
+
+    adafruit_response = http.request('GET', adafruit_temp_url, headers = adafruit_header)
+    adafruit_data = adafruit_response.data
+    adafruit_temp_value = json.loads(adafruit_data)['value']
+        
+    adafruit_response = http.request('GET', adafruit_rh_url, headers = adafruit_header)
+    adafruit_data = adafruit_response.data
+    adafruit_rh_value = json.loads(adafruit_data)['value']
+
+    adafruit_response = http.request('GET', adafruit_tulo_temp_url, headers = adafruit_header)
+    adafruit_data = adafruit_response.data
+    adafruit_tulo_temp_value = json.loads(adafruit_data)['value']
+
+    adafruit_response = http.request('GET', adafruit_tulo_rh_url, headers = adafruit_header)
+    adafruit_data = adafruit_response.data
+    adafruit_tulo_rh_value = json.loads(adafruit_data)['value']
+
+    return {
+        'tempIn':round(float(adafruit_temp_value), 1),
+        'rhIn':round(float(adafruit_rh_value)),
+        'tempTulo':round(float(adafruit_tulo_temp_value), 1),
+        'rhTulo':round(float(adafruit_tulo_rh_value))
+    }
